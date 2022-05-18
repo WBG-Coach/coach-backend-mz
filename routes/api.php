faaf3a3ba494393f56ad8e085e156a79e47de663
Airton Filho <airtonmfilho@hotmail.com>
2022-05-18 02:59:31 -0300

    Adding api routes do Search, create, edit and delete Schools, users and users profiles

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,23 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/schools/search', ['as' => 'schools.search', 'uses' => 'SchoolController@search']);
+Route::post('/schools', ['as' => 'schools.save', 'uses' => 'SchoolController@save']);
+Route::put('/schools', ['as' => 'schools.update', 'uses' => 'SchoolController@update']);
+Route::delete('/schools/{id}', ['as' => 'schools.delete', 'uses' => 'SchoolController@delete']);
+
+Route::post('/profiles/search', ['as' => 'profiles.search', 'uses' => 'ProfileController@search']);
+Route::post('/profiles', ['as' => 'profiles.save', 'uses' => 'ProfileController@save']);
+Route::put('/profiles', ['as' => 'profiles.update', 'uses' => 'ProfileController@update']);
+Route::delete('/profiles/{id}', ['as' => 'profiles.delete', 'uses' => 'ProfileController@delete']);
+
+Route::post('/users/search', ['as' => 'users.search', 'uses' => 'UserController@search']);
+Route::post('/users', ['as' => 'users.save', 'uses' => 'UserController@save']);
+Route::put('/users', ['as' => 'users.update', 'uses' => 'UserController@update']);
+Route::delete('/users/{id}', ['as' => 'users.delete', 'uses' => 'UserController@delete']);
+
+
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
