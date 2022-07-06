7d56f29c81e991e4919ebdab84e7dc4f12a412d5
unknown <airtonmfilho@hormail.com>
2022-07-06 13:17:17 -0300

    Creating migration to change all api_tokens

---
+++ database/migrations/2022_07_06_161556_change_api_tokens_from_all_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use Illuminate\Support\Str;
+use App\Models\User;
+
+class ChangeApiTokensFromAllUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach (User::all() as $user) {
+            $user->api_token = Str::random(80);
+            $user->update();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // NONE
+    }
+}
