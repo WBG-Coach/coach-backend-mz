21d2d08b8600e2cd5160c66856ab1b4857fbc9b8
unknown <airtonmfilho@hormail.com>
2022-07-06 11:45:26 -0300

    Changing loggin service to authenticate only if the user_profile is COACH

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,18 +7,21 @@ use Illuminate\Http\Request;
 use App\Http\Requests\Login\GetTokenRequest;
 
 use App\Models\User;
+use App\Models\Profile;
 
 class LoginController extends Controller
 {
     public function getToken(GetTokenRequest $request)
     {
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+        $profile = Profile::where('name', 'COACH')->first();
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'profile_id' => $profile->id])) {
             return \Auth::user();
         }
 
         return response()->json([
             'code'      =>  500,
             'message'   =>  'Authentication failed.'
-        ], 500); 
+        ], 500);
     }
 }
