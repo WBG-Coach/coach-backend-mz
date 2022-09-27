be7bfee2efce129424fff20f8418b207cc05f19b
Airton Filho <airtonmfilho@hotmail.com>
2022-09-26 21:55:28 -0300

    Changing answer controller to populate order field when save

--- app/Http/Controllers/QuestionnaireApplicationController.php
+++ app/Http/Controllers/QuestionnaireApplicationController.php
@@ -49,6 +49,7 @@ class QuestionnaireApplicationController extends Controller
                 $application->questionnaire_id = $project->observation_questionnaire_id;
                 $application->feedback_questionnaire_id = $project->feedback_questionnaire_id;
             }
+            $application->order = QuestionnaireApplication::where('teacher_id', $request->teacher_id)->count()+1;
             $application->save();
 
             \DB::commit();
