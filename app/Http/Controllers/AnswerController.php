21a186fda718a41151089cd0cfcc30e1b1c06225
airton <airtonmfilho@hotmail.com>
2022-10-24 09:57:05 -0300

    Adding service to get city from lat and long when saves answers

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -37,6 +37,51 @@ class AnswerController extends Controller
         return $search->get();
     }
 
+    public function getCity($lat, $long)
+    {
+        // Create a cURL handle
+        $ch = curl_init();
+
+        // define options
+        $optArray = array(
+            CURLOPT_URL => 'https://maps.googleapis.com/maps/api/geocode/json?latlng='.$lat.','.$long.'&key=AIzaSyBJR-Qm19jraWkc52MXazoQfMp5uBnZkUg',
+            CURLOPT_RETURNTRANSFER => true
+        );
+
+        // apply those options
+        curl_setopt_array($ch, $optArray);
+
+        // Execute
+        $response = curl_exec($ch);
+
+        // Check HTTP status code
+        if (!curl_errno($ch)) {
+        switch ($http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE)) {
+            case 200:  # OK
+
+            break;
+            default:
+            // Close handle
+            curl_close($ch);
+            abort(500, 'Unexpected HTTP code: '.$http_code);
+        }
+        }
+
+        // Close handle
+        curl_close($ch);
+
+        $city = "";
+        foreach (json_decode($response)->results[0]->address_components as $addressComponent) {
+            foreach ($addressComponent->types as $type) {
+                if ($type == 'administrative_area_level_2') {
+                    $city = $addressComponent->long_name;
+                }
+            }
+        }
+
+        return $city;
+    }
+
     public function save(StoreRequest $request)
     {
         \DB::beginTransaction();
@@ -66,6 +111,9 @@ class AnswerController extends Controller
                     foreach ($request->answers as $answer) {
                         $model = new Answer();
                         $model->fill($answer);
+                        if (isset($answer['latitude']) && isset($answer['longitude'])) {
+                            $model->city = $this->getCity($answer['latitude'], $answer['longitude']);
+                        }
                         $model->questionnaire_application_id = $application->id;
                         $model->questionnaire_id = $application->questionnaire_id;
                         $model->save();
--- app/Http/Controllers/DocumentQuestionnaireController.php
+++ app/Http/Controllers/DocumentQuestionnaireController.php
@@ -11,6 +11,51 @@ use App\Models\AnswerFile;
 class DocumentQuestionnaireController extends Controller
 {
 
+    public function getCity($lat, $long)
+    {
+        // Create a cURL handle
+        $ch = curl_init();
+
+        // define options
+        $optArray = array(
+            CURLOPT_URL => 'https://maps.googleapis.com/maps/api/geocode/json?latlng='.$lat.','.$long.'&key=AIzaSyBJR-Qm19jraWkc52MXazoQfMp5uBnZkUg',
+            CURLOPT_RETURNTRANSFER => true
+        );
+
+        // apply those options
+        curl_setopt_array($ch, $optArray);
+
+        // Execute
+        $response = curl_exec($ch);
+
+        // Check HTTP status code
+        if (!curl_errno($ch)) {
+        switch ($http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE)) {
+            case 200:  # OK
+
+            break;
+            default:
+            // Close handle
+            curl_close($ch);
+            abort(500, 'Unexpected HTTP code: '.$http_code);
+        }
+        }
+
+        // Close handle
+        curl_close($ch);
+
+        $city = "";
+        foreach (json_decode($response)->results[0]->address_components as $addressComponent) {
+            foreach ($addressComponent->types as $type) {
+                if ($type == 'administrative_area_level_2') {
+                    $city = $addressComponent->long_name;
+                }
+            }
+        }
+
+        return $city;
+    }
+
     public function save(Request $request)
     {
         \DB::beginTransaction();
@@ -22,6 +67,9 @@ class DocumentQuestionnaireController extends Controller
                 foreach ($request->answers as $answer) {
                     $model = new Answer();
                     $model->fill($answer);
+                    if (isset($answer['latitude']) && isset($answer['longitude'])) {
+                        $model->city = $this->getCity($answer['latitude'], $answer['longitude']);
+                    }
                     $model->questionnaire_id = $application->doc_questionnaire_id;
                     $model->questionnaire_application_id = $application->id;
                     $model->save();
