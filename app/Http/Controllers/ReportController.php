fc807293690747a14b771e5254fde2973d3aa8d1
airton <airtonmfilho@hotmail.com>
2022-12-13 12:37:32 -0300

    FIX: removing alias from feedbaks query into competenceEvolutions service from reports

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -31,7 +31,7 @@ class ReportController extends Controller
                 $finalDate = date('Y-m-01', strtotime('-'.($months - $i - 1).' month'));
 
                 $feedbackQty = Feedback::where('competence_id', $competence->id)
-                        ->whereRaw("(f.created_at >= '" .$startDate." 00:00:00' and f.created_at < '".$finalDate." 00:00:00')")
+                        ->whereRaw("(created_at >= '" .$startDate." 00:00:00' and created_at < '".$finalDate." 00:00:00')")
                         ->count();
                 
                 $monthlyData[$i] = ['month' => date('m/Y', strtotime('-'.($months - $i).' month')), 'qty' => $feedbackQty];
